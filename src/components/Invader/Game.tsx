fbf8ff55286ec98951a79efc97c04af1e1bc3085
Ryoko Kawashima <ry.sep27@gmail.com>
2019-10-10 10:37:05 +0900

    fix error

--- src/components/Invader/Game.tsx
+++ src/components/Invader/Game.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GameContainer } from '../Style'
 
-const Landing = () => {
+const Game = () => {
   return (
     <GameContainer style={{ backgroundColor: 'black' }}>
       <div>invader</div>
@@ -9,4 +9,4 @@ const Landing = () => {
   )
 }
 
-export default Landing
+export default Game
